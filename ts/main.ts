67c0957e1eb9a37265d3096568ce145d8266b9ea
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-05-01 11:11:31 -0700

    Styled web form and starting code

---
+++ ts/main.ts
@@ -0,0 +1,10 @@
+function main():void{
+    //make a method to validate letters
+        //if invalid we need to show a message in the errors-div
+    //make a method to validate numbers
+        //if invalid we need to show a message in the errors-div
+    //make a method to validate dates
+        //if invalid we need to show a message in the errors-div
+
+    
+}
