5d0137540e8d7f539811b2dc656acdf94983fc1c
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-05-02 16:15:35 -0700

    Finished errorMessage method

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
-function main() {
-}
+window.onload = function () {
+};
 function isValidString(id) {
     var validString = true;
     var inputBox = getEle(id);
     var inputBoxValue = inputBox.value;
     if (inputBoxValue.trim() == "") {
         validString = false;
+        errorMessage("You must enter a valid name", "#errors-div");
     }
     return validString;
 }
 function isValidNumber(id) {
-    var isValidNumber = true;
+    var validNumber = true;
     var inputBox = getEle(id);
     var inputBoxValue = inputBox.value;
     if (inputBoxValue.trim() == "") {
+        errorMessage("You must enter a valid price", "#errors-div");
         return false;
     }
-    if (isNaN(parseFloat(inputBoxValue))) {
+    else if (isNaN(parseFloat(inputBoxValue))) {
+        errorMessage("You must enter a valid price", "#errors-div");
         return false;
     }
-    return isValidNumber;
+    return validNumber;
 }
 function getEle(id) {
     return document.getElementById(id);
+}
+function errorMessage(errorMessage, element) {
+    var errorList = document.createElement("p");
+    errorList.innerText = errorMessage;
+    getEle(element).appendChild(errorList);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,18 @@
-function main():void{
+window.onload = function(){
+    
+}
     //make a method to validate letters
         //if invalid we need to show a message in the errors-div
         //use isValidString()
 
     //make a method to validate numbers
         //if invalid we need to show a message in the errors-div
+        //use isValidNumber()
 
     //make a method to validate dates
         //if invalid we need to show a message in the errors-div
-}
+
+
 
 /**
  * Returns true or false depending if the target elements value is a valid string
@@ -23,6 +27,7 @@ function isValidString(id:string):boolean{
     //if input is empty validString is false
     if(inputBoxValue.trim() == ""){
         validString = false;
+        errorMessage("You must enter a valid name", "#errors-div");
     }
     return validString;
 }
@@ -33,27 +38,49 @@ function isValidString(id:string):boolean{
  * @returns if the html input element is a valid number
  */
 function isValidNumber(id:string):boolean{
-    let isValidNumber = true;
+    let validNumber = true;
     let inputBox = <HTMLInputElement>getEle(id);
     let inputBoxValue = inputBox.value;
 
     //if the value is empty return false
     if(inputBoxValue.trim() == ""){
+        errorMessage("You must enter a valid price", "#errors-div");
         return false;
     }
     //if the value isNan return false
-    if(isNaN(parseFloat(inputBoxValue))){
+    else if(isNaN(parseFloat(inputBoxValue))){
+        errorMessage("You must enter a valid price", "#errors-div");
         return false;
     }
-    return isValidNumber;
+    return validNumber;
 }
 
+//maybe use this idea later
+// function isValidDate(id:string):boolean{
+//     let validDate = true;
+//     let inputBox = <HTMLInputElement>getEle(id);
+//     let inputBoxValue = inputBox.value;
+
+//     //use this regular expression to validate the date
+//     //  ^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
+// }
 
 /**
- * 
+ * A shortcut for document.getElementById();
  * @param id of the target element
  * @returns the target element
  */
 function getEle(id:string){
     return document.getElementById(id);
+}
+
+/**
+ * creates an element then displays an error message
+ * @param errorMessage the message you want to show
+ * @param element the parent of the element you wish to target 
+ */
+function errorMessage(errorMessage:string, element:string){
+    let errorList = <HTMLElement>document.createElement("p");
+    errorList.innerText = errorMessage;
+    getEle(element).appendChild(errorList);
 }
